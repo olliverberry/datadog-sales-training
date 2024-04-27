edb551921afa7a4213795a6db63ab42a93084d8d
stuartcmehrens <stuart.mehrens@datadoghq.com>
2024-04-26 21:33:02 -0400

    fix delete script

--- azure-integration/infrastructure/scripts/tear-down-azure.ps1
+++ azure-integration/infrastructure/scripts/tear-down-azure.ps1
@@ -1,5 +1,10 @@
 $ManagementGroupName = "datadog-sales-training"
-$managementGroup = Get-AzManagementGroup -GroupName $ManagementGroupName
+$managementGroup = Get-AzManagementGroup -GroupName $ManagementGroupName -ErrorAction SilentlyContinue
+if (-not $managementGroup) {
+    Write-Host "unable to find management group '$ManagementGroupName'."
+    exit 0
+}
+
 $subscriptions = Get-AzManagementGroupSubscription -GroupName $managementGroup.Name
 Write-Host "Got '$($subscriptions.Count)' subscriptions."
 if ($subscriptions.Count -eq 0) {
