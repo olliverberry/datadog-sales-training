5b2cdc68b41b06c12f591c06f8bf6262c357c938
stuartcmehrens <stuart.mehrens@datadoghq.com>
2024-04-29 10:07:34 -0400

    use subscription id

--- azure-integration/infrastructure/scripts/tear-down-azure.ps1
+++ azure-integration/infrastructure/scripts/tear-down-azure.ps1
@@ -1,32 +1,28 @@
-$ManagementGroupName = "datadog-sales-training"
-$managementGroup = Get-AzManagementGroup -GroupName $ManagementGroupName -ErrorAction SilentlyContinue
-if (-not $managementGroup) {
-    Write-Host "unable to find management group '$ManagementGroupName'. exiting."
+param(
+    [Parameter(Mandatory=$true)]
+    [string] $SubscriptionId
+)
+
+Write-Host "getting subscription '$SubscriptionId'."
+$subscription = Get-AzSubscription -SubscriptionId $SubscriptionId -ErrorAction SilentlyContinue
+if (-not $subscription) {
+    Write-Host "unable to find subscription '$SubscriptionId'. exiting."
     exit 0
 }
 
-Write-Host "getting subscriptions for management group '$($managementGroup.Name)'."
-$subscriptions = Get-AzManagementGroupSubscription -GroupName $managementGroup.Name
-Write-Host "got '$($subscriptions.Count)' subscriptions."
-if ($subscriptions.Count -eq 0) {
-    exit 0
-}
-
-foreach ($subscription in $subscriptions) {
-    Write-Host "getting role assignments for subscription '$($subscription.Id)'."
-    $roleAssignments = Get-AzRoleAssignment -Scope $subscription.Id
-    foreach ($roleAssignment in $roleAssignments) {
-        if ($roleAssignment.SignInName -like "*user*" -and $roleAssignment.ObjectType -eq "User") {
-            Write-Host "removing user '$($roleAssignment.SignInName)'"
-            Remove-AzADUser -ObjectId $roleAssignment.ObjectId
-            Start-Sleep -Seconds 15
-        }
-    }
-
-    Set-AzContext -Subscription $subscription.Id
-    $resourceGroups = Get-AzResourceGroup
-    foreach ($resourceGroup in $resourceGroups) {
-        Remove-AzResourceGroup -Id $resourceGroup.ResourceId -Force
+Write-Host "getting role assignments for subscription '$($subscription.Id)'."
+$roleAssignments = Get-AzRoleAssignment -Scope $subscription.Id
+foreach ($roleAssignment in $roleAssignments) {
+    if ($roleAssignment.SignInName -like "*user*" -and $roleAssignment.ObjectType -eq "User") {
+        Write-Host "removing user '$($roleAssignment.SignInName)'"
+        Remove-AzADUser -ObjectId $roleAssignment.ObjectId
         Start-Sleep -Seconds 15
     }
+}
+
+Set-AzContext -Subscription $subscription.Id
+$resourceGroups = Get-AzResourceGroup
+foreach ($resourceGroup in $resourceGroups) {
+    Remove-AzResourceGroup -Id $resourceGroup.ResourceId -Force
+    Start-Sleep -Seconds 15
 }
